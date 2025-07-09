43f5ac05dde4affb7988df3cd51f3eb8f9bd8866
nup-agarwal <nupuragarwal915@gmail.com>
2025-07-09 16:28:00 +0530

    Create UserController.java

---
+++ UserController.java
@@ -0,0 +1,27 @@
+package com.wellsfargo.controllers;
+
+import com.wellsfargo.entities.User;
+import com.wellsfargo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
