491e25a941a52bdbe65604d246990fd873afd822
nup-agarwal <nupuragarwal915@gmail.com>
2025-07-09 16:26:06 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,47 @@
+package com.wellsfargo.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    // Constructor
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    // Default constructor
+    public User() {}
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
